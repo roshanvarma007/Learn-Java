7c508b9e2fbf9e2e2780a0dbaa6b3069ad73e2e6
roshanvarma007 <133495723+roshanvarma007@users.noreply.github.com>
2023-05-13 05:26:04 -0700

    Add files via upload

---
+++ InputScanner.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;    //first we  have to import java
+
+public class InputScanner {
+
+    public static void main(String[] args) {
+
+       String str = "Please Entre Your Name , Age and Salary";
+        System.out.println(str);
+ Scanner scan = new Scanner(System.in);
+     String name = scan.nextLine();
+       int age = scan.nextInt();
+       float salary = scan.nextFloat();
+       System.out.println("******");
+        System.out.println("Your name is"+ " " + name);
+        System.out.println("Your age is"+ " " +age);
+        System.out.println("Your salary"+ " " +salary);
+        System.out.println("******");
+        
+    }
+
+    
+}
