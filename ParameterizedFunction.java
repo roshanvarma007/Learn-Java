07d601f7cb6edf1f5159a18486e9dc2066fda947
roshanvarma007 <133495723+roshanvarma007@users.noreply.github.com>
2023-05-16 08:37:01 -0700

    Add files via upload

---
+++ ParameterizedFunction.java
@@ -0,0 +1,26 @@
+public class ParameterizedFunction { // parameter is also called a agrs or argument
+    public static void main(String[] args) {
+
+        int user1Age = 20;
+        getAgeInDays(user1Age);
+
+        int user2Age = 15;
+        getAgeInDays(user2Age);
+
+        int ammout = 1000;
+        int time = 5;
+        int rate = 12;
+        calculateIntrestRate(ammout, time, rate);
+
+    }
+
+    public static void getAgeInDays(int age) {
+        System.out.println(age * 365);
+
+    }
+
+    public static void calculateIntrestRate(int ammout, int time, int rate) {
+        System.out.println(ammout * time * rate / 100);
+
+    }
+}
